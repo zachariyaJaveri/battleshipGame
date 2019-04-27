e0e20ad286368162679078b05a4432990d393ee2
Zachariya Javeri <zach.javeri@gmail.com>
2019-04-27 01:29:06 -0400

    Added navigation screen and stuff

---
+++ Battleship/Battleship/Game/Board.swift
@@ -0,0 +1,29 @@
+//
+//  Board.swift
+//  Battleship
+//
+//  Created by Zachariya Javeri on 2019-04-27.
+//  Copyright © 2019 Lister, Julia. All rights reserved.
+//
+
+import UIKit
+
+class Board: UIView, drawableBoard {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    func drawBoard() {
+        //I don't know what type of board this is so don't draw it
+        return
+    }
+}
+
+protocol drawableBoard {
+    func drawBoard()
+}
